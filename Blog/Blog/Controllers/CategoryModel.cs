9f45940844dc12824e596facf6276c9ae3d990cc
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-05-06 14:54:54 -0400

    Iniciando o CRUD de categorias

---
+++ Blog/Blog/Controllers/CategoryModel.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    public class CategoryModel : ControllerBase
+    {
+
+        [HttpGet("categories")]
+        public IActionResult Get([FromServices] BlogDataContext context)
+        {
+            var categories = context.Categories.ToList();
+            return Ok();
+        }
+
+    }
+}
--- Blog/Blog/Models/User.cs
+++ Blog/Blog/Models/User.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
-namespace Blog.Models
+﻿namespace Blog.Models
 {
     public class User
     {
