36c2ad5bbd5c6e75a2f693df3812849d2bd49bde
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-05-06 14:36:55 -0400

    disponibilizando o contexto como serviço

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -1,8 +1,11 @@
+using Blog.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BlogDataContext>(); // disponibilizando o contexto como serviço
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
